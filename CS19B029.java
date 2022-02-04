e3cc9a94c81af614373068925c6cabde345d1c31
nmnsharma007 <nmnsharma20@gmail.com>
2022-02-04 23:35:16 +0530

    parsing and executing intermediate code

--- CS19B029.java
+++ CS19B029.java
@@ -40,27 +40,70 @@ class QueryParser {
 
 }
 
+class RuntimeEngine{
+
+    private Table createTable(String token){
+        String tabname = token.split(" ")[1];
+        Table table = new Table();
+        table.tabname = tabname;
+        return table;
+    }
+
+    private void addAttribute(String token){
+        String type = token.split(" ")[1];
+        String name = token.split(" ")[2];
+
+    }
+
+    public void executeCode(String code){
+        StringTokenizer sTokenizer = new StringTokenizer(code,"\n");
+        while(sTokenizer.hasMoreTokens()){
+            String token = sTokenizer.nextToken();
+            if(token.startsWith("create_table")){
+                Table tb = createTable(token);
+            }
+            else if(token.startsWith("insert_into")){
+
+            }
+            else if(token.startsWith("add_attribute")){
+                addAttribute(token);
+            }
+            else if(token.startsWith("save_tab")){
+
+            }
+            else if(token.startsWith("exists_tab")){
+
+            }
+        }
+    }
+}
+
 public class CS19B029 {
 
-    public static void main(String[] args) {
-       BufferedReader bReader;
-       try {
-           bReader = new BufferedReader(new FileReader("cs19b029.query"));
-           String query = "";
-           String contentLine = bReader.readLine();
-           while(contentLine != null){
+    private static String getQuery(String progfile){
+        BufferedReader bReader;
+        String code = "";
+        try {
+            bReader = new BufferedReader(new FileReader(progfile));
+            String query = "";
+            String contentLine = bReader.readLine();
+            while(contentLine != null){
                 query += contentLine + "\n";
                 contentLine = bReader.readLine();// read the commands line by line
-           }
-           QueryParser queryparser = new QueryParser();
-           String code = queryparser.parser(query);
-           BufferedWriter bWriter = new BufferedWriter(new FileWriter("cs19b029.query.code"));
-           bWriter.write(code);
-           bWriter.close();
-           bReader.close();
-       }
-       catch(IOException e){
-           e.printStackTrace();
-       }
+            }
+            QueryParser queryparser = new QueryParser();
+            code = queryparser.parser(query);
+            bReader.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        return code;
+    }
+
+    public static void main(String[] args) {
+       String intermediateCode = getQuery("prog1.txt");
+       RuntimeEngine engine = new RuntimeEngine();
+       engine.executeCode(intermediateCode);// execute the steps of the intermediate code
     }
 }
---
+++ Table.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Table {
+    
+    public String tabname;
+    public ArrayList<Attribute> attr_list;
+
+    Table() {
+        attr_list = new ArrayList<>();
+    }
+}
